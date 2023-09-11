9ca91d89a454b30c23c913899370f81d11cb1072
leonardodalinky <linkyy2000313@gmail.com>
2023-09-11 21:06:30 +0800

    misc: readme

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # fpsample
+[![pypi package version badge](https://img.shields.io/pypi/v/fpsample)](https://pypi.org/project/fpsample/)
+![python version badge](https://img.shields.io/badge/python-%3E%3D3.7-blue)
+[![license badge](https://img.shields.io/github/license/leonardodalinky/fpsample)](https://github.com/leonardodalinky/fpsample/blob/main/LICENSE)
+[![star badge](https://img.shields.io/github/stars/leonardodalinky/fpsample?style=social)](https://github.com/leonardodalinky/fpsample)
+
 Python farthest point sampling library built on Rust pyo3 bindings. Compatible with numpy.
 
 ## Installation
@@ -16,4 +21,49 @@ The library is built using [maturin](https://github.com/PyO3/maturin). Ensure yo
 Build the library and install using:
 ```shell
 maturin develop --release
+```
+
+## Usage
+
+```python
+import fpsample
+import numpy as np
+
+# Generate random point cloud
+pc = np.random.rand(4096, 3)
+# sample 1024 points
+fps_samples = fpsample.fps_sampling(pc, 1024)
+
+fps_npdu_samples = fpsample.fps_npdu_sampling(pc, 1024)
+# or specify the windows size
+fps_npdu_samples = fpsample.fps_npdu_sampling(pc, 1024, k=64)
+```
+
+> NOTE: NPDU method only gives sub-optimal answers. And it assumes that the points are approximately sorted or have **dimensional locality**. Otherwise, the result may be **worse**. Check the paper for details.
+
+## Performance
+Setup:
+  - CPU: Intel(R) Core(TM) i9-10940X CPU @ 3.30GHz
+  - RAM: 128 GiB
+
+|  Method  | #samples | #points |        Time       |
+|:--------:|:--------:|:-------:|:-----------------:|
+|    FPS   |   1024   |   4096  |  18.6 ms ± 1.3 ms |
+| FPS+NPDU |   1024   |   4096  | 5.97 ms ± 0.61 ms |
+|    FPS   |  50,000  | 100,000 |  27.4 s ± 551 ms  |
+| FPS+NPDU |  50,000  | 100,000 |  5.36 s ± 152 ms  |
+
+## Reference
+The nearest-point-distance-updating (NPDU) heuristic strategy is proposed in the following paper:
+```
+@INPROCEEDINGS{9919246,
+  author={Li, Jingtao and Zhou, Jian and Xiong, Yan and Chen, Xing and Chakrabarti, Chaitali},
+  booktitle={2022 IEEE Workshop on Signal Processing Systems (SiPS)},
+  title={An Adjustable Farthest Point Sampling Method for Approximately-sorted Point Cloud Data},
+  year={2022},
+  volume={},
+  number={},
+  pages={1-6},
+  doi={10.1109/SiPS55645.2022.9919246}
+}
 ```
--- python/fpsample/wrapper.py
+++ python/fpsample/wrapper.py
@@ -5,41 +5,41 @@ import numpy as np
 from .fpsample import _fps_npdu_sampling, _fps_sampling
 
 
-def fps_sampling(pcd_xyz: np.ndarray, n_samples: int) -> np.ndarray:
+def fps_sampling(pc: np.ndarray, n_samples: int) -> np.ndarray:
     """
     Args:
-        pcd_xyz (np.ndarray): The input point cloud of shape (n_pts, 3).
+        pc (np.ndarray): The input point cloud of shape (n_pts, D).
         n_samples (int): Number of samples.
     Returns:
         np.ndarray: The selected indices of shape (n_samples,).
     """
     assert n_samples >= 1, "n_samples should be >= 1"
-    assert pcd_xyz.ndim == 2
-    n_pts, _ = pcd_xyz.shape
+    assert pc.ndim == 2
+    n_pts, _ = pc.shape
     assert n_pts >= n_samples, "n_pts should be >= n_samples"
-    pcd_xyz = pcd_xyz.astype(np.float32)
+    pc = pc.astype(np.float32)
     # best performance with fortran array
-    pcd_xyz = np.asfortranarray(pcd_xyz)
+    pc = np.asfortranarray(pc)
     # Random pick a start
     start_idx = np.random.randint(low=0, high=n_pts)
-    return _fps_sampling(pcd_xyz, n_samples, start_idx)
+    return _fps_sampling(pc, n_samples, start_idx)
 
 
-def fps_npdu_sampling(pcd_xyz: np.ndarray, n_samples: int, k: Optional[int] = None) -> np.ndarray:
+def fps_npdu_sampling(pc: np.ndarray, n_samples: int, k: Optional[int] = None) -> np.ndarray:
     """
     Args:
-        pcd_xyz (np.ndarray): The input point cloud of shape (n_pts, 3).
+        pc (np.ndarray): The input point cloud of shape (n_pts, D).
         n_samples (int): Number of samples.
         k (int, default=None): Windows size of local heuristic search. If set to None, it will be set to `n_pts / n_samples * 16`.
     Returns:
         np.ndarray: The selected indices of shape (n_samples,).
     """
     assert n_samples >= 1, "n_samples should be >= 1"
-    assert pcd_xyz.ndim == 2
-    n_pts, _ = pcd_xyz.shape
+    assert pc.ndim == 2
+    n_pts, _ = pc.shape
     assert n_pts >= n_samples, "n_pts should be >= n_samples"
-    pcd_xyz = pcd_xyz.astype(np.float32)
+    pc = pc.astype(np.float32)
     k = k or int(n_pts / n_samples * 16)
     # Random pick a start
     start_idx = np.random.randint(low=0, high=n_pts)
-    return _fps_npdu_sampling(pcd_xyz, n_samples, k, start_idx)
+    return _fps_npdu_sampling(pc, n_samples, k, start_idx)
