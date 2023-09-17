7d264cffa8e24c620c8c92aa6e5618ab81963d64
leonardodalinky <linkyy2000313@gmail.com>
2023-09-17 17:13:14 +0800

    fix: force cpp link lib

--- build.rs
+++ build.rs
@@ -3,8 +3,8 @@ mod build_info {
 }
 
 fn main() {
-    cc::Build::new()
-        .file("src/bucket_fps/c_wrapper.cpp")
+    let mut b = cc::Build::new();
+    b.file("src/bucket_fps/c_wrapper.cpp")
         .define(
             "BUCKET_FPS_MAX_DIM",
             build_info::BUCKET_FPS_MAX_DIM.to_string().as_str(),
@@ -12,6 +12,11 @@ fn main() {
         .cpp(true)
         .std("c++17")
         .warnings(false)
-        .include("src/bucket_fps/_ext")
-        .compile("bucketfps");
+        .include("src/bucket_fps/_ext");
+
+    if let Ok(cpp_lib) = std::env::var("FORCE_CXXSTDLIB") {
+        b.cpp_set_stdlib(cpp_lib.as_str());
+    }
+
+    b.compile("bucketfps");
 }
