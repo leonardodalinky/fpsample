61fa1ea6b51010a866a055475a60b27e6ca29a59
leonardodalinky <linkyy2000313@gmail.com>
2023-09-17 16:30:57 +0800

    fix: DIM=0 dropped from compile time

--- src/bucket_fps/c_wrapper.cpp
+++ src/bucket_fps/c_wrapper.cpp
@@ -67,7 +67,7 @@ using KDLineFuncType = void (*)(const float *, size_t, size_t, size_t, size_t,
 
 template <typename T, size_t Count, typename M, size_t... I>
 constexpr std::array<T, Count> mapIndices(M &&m, std::index_sequence<I...>) {
-    std::array<T, Count> result{m.template operator()<I>()...};
+    std::array<T, Count> result{m.template operator()<I + 1>()...};
     return result;
 }
 
@@ -84,7 +84,7 @@ template <typename T, typename S = T> struct kdtree_func_helper {
 
 template <typename T, typename S = T> struct kdline_func_helper {
     template <size_t DIM> KDLineFuncType operator()() {
-        return DIM == 0 ? nullptr : &kdline_sample<T, DIM, S>;
+        return &kdline_sample<T, DIM, S>;
     }
 };
 
@@ -105,10 +105,9 @@ int bucket_fps_kdtree(const float *raw_data, size_t n_points, size_t dim,
         // start_idx should be smaller than n_samples
         return 2;
     }
-    auto func_arr =
-        map<KDTreeFuncType, max_dim + 1>(kdtree_func_helper<float>{});
-    func_arr[dim](raw_data, n_points, dim, n_samples, start_idx,
-                  sampled_point_indices);
+    auto func_arr = map<KDTreeFuncType, max_dim>(kdtree_func_helper<float>{});
+    func_arr[dim - 1](raw_data, n_points, dim, n_samples, start_idx,
+                      sampled_point_indices);
     return 0;
 }
 
@@ -122,10 +121,9 @@ int bucket_fps_kdline(const float *raw_data, size_t n_points, size_t dim,
         // start_idx should be smaller than n_samples
         return 2;
     }
-    auto func_arr =
-        map<KDLineFuncType, max_dim + 1>(kdline_func_helper<float>{});
-    func_arr[dim](raw_data, n_points, dim, n_samples, start_idx, height,
-                  sampled_point_indices);
+    auto func_arr = map<KDLineFuncType, max_dim>(kdline_func_helper<float>{});
+    func_arr[dim - 1](raw_data, n_points, dim, n_samples, start_idx, height,
+                      sampled_point_indices);
     return 0;
 }
 }
